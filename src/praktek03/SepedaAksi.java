83989e414c1aee2adc228c3588e7e12d8139e427
AhmadRendyFirjatullah <a.rendyfirjatullah@gmail.com>
2023-05-22 16:20:04 +0800

    Buat class Aksi

---
+++ src/praktek03/SepedaAksi.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktek03;
+
+public class SepedaAksi {
+    public static void main(String[] args) {
+        
+    }
+}
